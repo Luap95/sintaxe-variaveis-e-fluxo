573ad067ff06ff674a815120e544dc7de491dc3b
Luap95 <lucas.7795.la@gmail.com>
2023-02-08 20:54:08 -0300

    Exercicíos de variaveis e conversões

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,18 @@
+public class TestaVariaveis {
+    public static void main(String[] args){
+        System.out.println("olá novo teste");
+
+        int idade;
+        idade = 27;
+
+        System.out.println("A minha idade é "+idade+" anos");
+
+        idade = 30 + 10;
+
+        System.out.println(idade);
+
+        idade = (7 * 5) / 2;
+
+        System.out.println(idade);
+    }
+}
